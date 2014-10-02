3ec28d609802cf630387832c17c654b280325375
Jason Marley <jmarley@redhat.com>
2014-10-02 11:42:19 -0400

    destoryed old repo and created new

---
+++ certBuilder.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+import paramiko
+
+class certBuilder:
+	def __init__(self, host, refcode, authcode, dir, env, alias, keystorePass, hostUser, hostPasswd):
+		self.host = host
+		self.refcode = refcode
+		self.authcode = authcode
+		self.env = env
+		self.dir = dir
+		self.keystore = dir + '/' + self.host + '.keystore' 
+		self.logfile = dir + '/' + self.host  + '.log'
+		self.csrfile = dir + '/' + self.host + '.csr'
+		cerfile = dir + '/' + self.host + '.cer'
+		self.alias = alias
+		self.passwd = keystorePass
+		self.hostUser = hostUser
+		self.hostPasswd = hostPasswd
+
+		if 'prod' in self.env:
+			rootcafile = '/opt/app/ProjectCerts/us_treas_ca.cer'
+                else:
+			rootcafile = '/opt/app/ProjectCerts/us_treas_ca_dev.cer'
+			ociocafile = '/opt/app/ProjectCerts/ocio_ca_dev.cer'
+			
+		# set keystore create cmd
+		self.keystoreCmd = ("keytool -genkeypair -v -alias " + self.alias 
+			+ " -keyalg RSA  -keysize 2048 -dname " + "'" + "cn=" 
+			+ self.host + ",ou=Devices, ou=Internal Revenue Service, ou=Department of the Treasury,o=U.S. Government,c=US" 
+			+ "'"  + " -keypass " + self.passwd + " -keystore " 
+			+ self.keystore + " -storepass " + self.passwd)
+
+		# set CSR cmd
+		self.csrCmd = ("keytool -certreq -keypass " + self.passwd 
+			+ " -keystore " + self.keystore + " -storepass " 
+			+ self.passwd + " -alias " + self.alias + " -file " 
+			+ self.csrfile)
+		
+		# set import root ca certificate cmd
+		self.importRootCaCmd = ("keytool -importcert -trustcacerts -keypass " 
+			+ self.passwd + " -keystore " + self.keystore + " -storepass " 
+			+ self.passwd + " -alias root_ca -file " + rootcafile 
+			+ " -noprompt")
+
+		# set import root ca certificate cmd
+		self.importOcioCaCmd = ("keytool -importcert -trustcacerts -keypass " 
+			+ self.passwd + " -keystore " + self.keystore + " -storepass " 
+			+ self.passwd + " -alias ocio_ca -file " + ociocafile 
+			+ " -noprompt")
+		
+		# set import signed certificate from ca cmd
+		self.importCerCmd = ("keytool -importcert -file " + cerfile 
+			+ " -alias " + self.alias + " -keypass " + self.passwd 
+			+ " -keystore " + self.keystore + " -storepass " 
+			+ self.passwd + " -noprompt")		
+		
+	def createKeystore(self):
+
+		try:
+			
+			# create log file
+			self.logHeader()
+
+			# create keystore
+			p = subprocess.call(self.keystoreCmd, shell=True)
+
+			with open(self.logfile, 'a') as logger:
+				if p == 1:
+					logger.write('\nfailured to create keystore\n')
+				else:
+					logger.write('Executed successfully\n\n')
+
+				logger.write("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
+		except IOError:
+			print('there is an i/o issue: ' )
+	
+	def exportCSR(self):
+		try:
+			# export csr
+			p = subprocess.call(self.csrCmd, shell=True)
+			
+			# append csr to log file
+	                with open(self.logfile, 'a') as logger:
+			
+				logger.write("Certificate Signing Request: ")
+			
+				
+				if p == 1:
+					logger.write('failured to create keystore\n')
+				else:
+					logger.write('Executed successfully\n\n')
+				
+				# log csr file created
+				logger.write(open(self.csrfile).read())
+				
+				logger.write("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
+		except IOError:
+			print('there is an i/o issue: ' )
+		
+	
+	def verifyParms(self):
+		try:
+			# verify fields are present
+			if not (self.host and self.refcode and self.authcode and self.env and self.dir):
+				return False
+			# test directory
+			if not os.path.isdir(self.dir):
+				os.makedirs(self.dir)	
+			# verify keystore doesnt' alreay exist
+			if os.path.isfile(self.keystore):
+				print("Keystore exists for this host: " + self.host + "in this path: " + self.dir)
+				return False
+			return True
+		except IOError:
+			print('Error with fields, please verify: ' +  IOError)
+
+	def importCER(self):
+		try:
+			
+			# create/open log file
+                        with open(self.logfile, 'a') as logger:
+			
+				logger.write("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+				logger.write("importing root ca certificate\n")
+			
+				# import root ca to keystore
+				p = subprocess.call(self.importRootCaCmd, shell=True)
+					
+				if p == 1:
+					logger.write('\n****ERROR****\nfailure to import root ca cert(s)\n')
+		
+				# import ocio ca to keystore
+				p = subprocess.call(self.importOcioCaCmd, shell=True)
+			
+				if p == 1:
+					logger.write('\n****ERROR****\nfailure to import ocio ca cert(s)\n')
+			
+				# import ca signed cert to keystore
+				p = subprocess.call(self.importCerCmd, shell=True)
+	
+				if p == 1:
+					logger.write('\n****ERROR****\nfailure to import signed ca cert\n')
+				else:
+					logger.write('\n****Successful import****\n')	
+				logger.write("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
+		except:
+			print('There is an i/o error attempting import CER' )
+
+		
+	def logHeader(self):
+		try:
+			with open(self.logfile, 'w') as logger:
+
+				# log file header
+	                        logger.write("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+	                        logger.write("Hostname: " + self.host)
+	                        logger.write("\nReference number: " + self.refcode)
+	                        logger.write("\nAuthCode: " +  self.authcode)
+	                        logger.write("\nAlias: " + self.alias)
+	                        logger.write("\n\nKeystore CMD\n")
+		                logger.write(self.keystoreCmd)
+	                        logger.write("\n\nExport CSR CMD\n")
+	                        logger.write(self.csrCmd)
+	                        logger.write("\n\nOcio cert import CMD\n")
+	                        logger.write(self.importOcioCaCmd)
+	                        logger.write("\n\nRoot cert import CMD\n")
+	                        logger.write(self.importRootCaCmd)
+	                        logger.write("\n\nImport signed cert CMD\n")
+	                        logger.write(self.importCerCmd)
+	                        logger.write("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+	                        logger.write("\n\nKeystore listed\n\n")
+		except:
+			print ('Error attempting to write to log file, please check')	
+	def scpKeystore(self):
+
+		try:
+
+			# directory we're going to write keystore to
+			scpFile = '/opt/app/SSL/' + self.host + '.keystore'
+
+			with open(self.logfile, 'a') as logger:
+				
+				logger.write("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+				logger.write("Secure copying keystore\n")
+				
+				try:
+					# instantiate host server as transport
+					transport = paramiko.Transport(self.host)
+					# give host params
+					transport.connect(username=self.hostUser,password=self.hostPasswd)									
+					# make connection
+					sftp = paramiko.SFTPClient.from_transport(transport)
+					
+					sftp.put(self.keystore,scpFile,confirm=False)
+					
+				except IOError:
+					print("couldn't sucessfully make a connection, please verify params")
+				logger.write('\n****Keystore successfully remote copied****\n')	
+				logger.write("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+		except IOError:
+			print('There is an issue with secure copying file, please verify')
+
---
+++ unittestCertBuilder.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+
+#
+# Author: Jason Marley
+# Date: 09/26/2014
+#
+
+import unittest
+import certBuilder
+import subprocess
+import shutil
+import re
+import os
+import paramiko
+import getpass
+
+class testCertFunctions(unittest.TestCase):
+
+	def setUp(self):
+		
+		testDir = '/home/jmarley/projects/federal/irs/workspace/automate-builtCerts/test'
+		#os.mkdir(testDir)
+		# build test 
+		self.cert = certBuilder.certBuilder('my.example.com',
+			'AAA-2222','111222333', testDir, 'dev','testAlias','kstorePassword','hostUser','hostPasswd')
+		
+		# verify Params
+		self.cert.verifyParms()	 
+
+	def tearDown(self):
+ 
+		#remove the test folder
+		shutil.rmtree(self.cert.dir)		
+		
+		# clear object 
+		self.cert = None       
+	
+	def test_createKeystore(self):
+
+		# create certificate/keystore		
+		self.cert.createKeystore()
+		
+		# set test keystore cmd
+		testKeystoreCmd = ("keytool -list -alias " +  
+			self.cert.alias + " -keypass " + 
+			self.cert.passwd + " -keystore " + 
+			self.cert.keystore + " -storepass " 
+			+ self.cert.passwd )
+		
+		# attempt to list the keystore contents
+		p = subprocess.call(testKeystoreCmd, shell=True)
+		
+		# assert no issues with command above
+		self.assertEqual(0,p) 
+		
+
+	def test_exportCSR(self):
+
+		# create certificate/keystore		
+		self.cert.createKeystore()
+	
+		# export csr from keystore		
+		self.cert.exportCSR()
+		
+		# test csr export cmd
+		p = os.path.isfile(self.cert.csrfile)
+
+		# assert no issues with command above
+		self.assertEqual(1,p)
+			
+	def test_importCER(self):
+	
+		certfile = self.cert.dir + '/' + self.cert.host + '.cer' 
+		cajks = self.cert.dir + '/ca.jks' 
+		catrust = self.cert.dir + '/truststore.jks'
+		cacert = self.cert.dir + '/ca.cer'
+
+		# create CA keystore
+		caKeystoreCmd=("keytool -genkeypair -keystore " + cajks + 
+			" -alias ca -dname cn=ca -storepass passwd -keypass passwd -validity 1 -keysize 1024 -keyalg RSA")
+	
+		p = subprocess.call(caKeystoreCmd,shell=True)
+		if p == 1:
+			print('cakeystorecmd issue')
+	
+		# export CA cert
+		caExportCerCmd = ("keytool -exportcert -alias ca -keystore " + cajks + " -storepass passwd -file " + cacert)
+	
+		p = subprocess.call(caExportCerCmd,shell=True)	
+		if p == 1:
+			print('caExportCerCmd issue')
+
+		# import CA cert into self
+		caImportCaCmd = (" keytool -importcert -keystore " + catrust + 
+			" -storepass passwd -alias ca -trustcacerts -file " + 
+			cacert + " -noprompt")
+
+		p = subprocess.call(caImportCaCmd,shell=True)
+		if p == 1:
+			print('caImportCaCmd issue')
+				
+		# create certificate/keystore
+		self.cert.createKeystore()
+		
+		# export csr from keystore
+                self.cert.exportCSR()
+
+		# export signed cert from CA 
+		caCreateCerCmd = (" keytool -gencert -infile " + self.cert.csrfile + 
+			" -outfile " + certfile + " -keystore " + cajks + 
+			" -alias ca -storepass passwd -keypass passwd -validity 1") 
+
+		# override importRootCaCmd
+		self.cert.importRootCaCmd = ("keytool -importcert -trustcacerts -keypass "
+                        + self.cert.passwd + " -keystore " + self.cert.keystore + " -storepass "
+                        + self.cert.passwd + " -alias root_ca -file " + cacert  + " -noprompt")
+
+		# override importOcioCaCmd 
+		self.cert.importOcioCaCmd = ("echo import OCIO not used ")
+		
+		p = subprocess.call(caCreateCerCmd,shell=True)		
+		if p == 1:
+                        print('caCreateCerCmd issue')
+			
+		# import signed cert
+		self.cert.importCER()
+
+		# get cert's sha1 fingerprint for exported cert from ca
+		caFingerprintCmd = "keytool -printcert -file " + cacert + " | grep 'SHA1'" 		
+		
+		result = subprocess.Popen(caFingerprintCmd, shell=True, stdout=subprocess.PIPE) 
+	
+		caFingerprint = re.split('SHA1: ', result.stdout.readline().strip())[1]	
+		
+		# get sha1 fingerprints from keystore
+		keystoreFingerprintCmd = ("keytool -list -storepass " + self.cert.passwd +
+			" -keystore " + self.cert.keystore + " | grep 'SHA1'") 	
+			
+		result2 = subprocess.Popen(keystoreFingerprintCmd, shell=True, stdout=subprocess.PIPE) 
+		
+		keystoreFingerprint = re.split('SHA1\):[ ]', result2.stdout.readline().strip())[1] 
+
+		self.assertEqual(keystoreFingerprint,caFingerprint)
+
+	def test_scp(self):
+		
+		# create keystore
+		self.cert.createKeystore()
+		
+		# get server connection info
+		self.cert.host = raw_input('enter test server ip: ')	
+		self.cert.hostUser = raw_input('server user name: ')
+		self.cert.hostPasswd = getpass.getpass()
+
+		# secure copy keystore
+		self.cert.scpKeystore()
+		
+		# directory keystore copied to
+		wdir = '/opt/app/SSL/'
+		keystore = self.cert.host + '.keystore'	
+
+		# instantiate up transport object 
+		transport = paramiko.Transport(self.cert.host)
+		
+		# add connection params
+		transport.connect(username=self.cert.hostUser,password=self.cert.hostPasswd) 		
+		
+		# make connection
+		sftp= paramiko.SFTPClient.from_transport(transport)
+
+		# list directory contnents
+		listDir = sftp.listdir(wdir)
+		
+		# does keystore exist	
+		scpSuccess = self.cert.host + '.keystore' in listDir
+
+		# remove added remote file
+		if scpSuccess:
+			sftp.remove(wdir + keystore)
+		
+		sftp.close()
+
+		self.assertTrue(scpSuccess)
+#	def test_eapCfg(self)
+
+if __name__ == '__main__':
+	unittest.main()
